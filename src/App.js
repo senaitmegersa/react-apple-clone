60a14496c1825c4870e963d22a193c7b5dd775df
Seni0215 <97817727+Seni0215@users.noreply.github.com>
2022-04-03 13:39:02 -0400

    Apple React Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './commonResource/css/styles.css';
+import Header from './commonResource/Header/Header';
+import Main from './commonResource/Main/Main';
+import First from './commonResource/First/First';
+import Second from './commonResource/Second/Second';
+import Third from './commonResource/Third/Third';
+import Fourth from './commonResource/Fourth/Fourth';
+import Fifth from './commonResource/Fifth/Fifth';
+import Sixth from './commonResource/Sixth/Sixth'
+import Footer from './commonResource/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+  <Header/>
+    <Main/>
+    <First/>
+    <Second/>
+    <Third/>
+    <Fourth/>
+    <Fifth/>
+    <Sixth/>
+    <Footer/>
+      </div>
   );
+ 
 }
 
 export default App;
+
+
